132fbcc77369153a4255e2235011abc9c3ac11b5
sasumi <cnsasumi@gmail.com>
2021-07-22 23:56:33 +0800

    cmt
    fix mysql charset dashes

--- src/Database/MySQL.php
+++ src/Database/MySQL.php
@@ -19,6 +19,18 @@ class MySQL extends DSN {
 		return 'mysql';
 	}
 
+	/**
+	 * fix charset dashes
+	 * @param $name
+	 * @param $value
+	 */
+	public function __set($name, $value){
+		if(strcasecmp($name, 'charset') === 0){
+			$value = str_replace('-', '', $value);
+		}
+		parent::__set($name, $value);
+	}
+
 	public static function getFieldMap(){
 		return [
 			'unix_socket' => 'unix_socket',
